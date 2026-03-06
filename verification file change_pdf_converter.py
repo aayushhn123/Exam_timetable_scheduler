6df5e13e7f64d1a05d7544db935d86ae70b8d7f6
Aayush Nayak <88595661+aayushhn123@users.noreply.github.com>
2026-03-06 20:49:27 +0530

    Update verification file change_pdf_converter.py

--- verification file change_pdf_converter.py
+++ verification file change_pdf_converter.py
@@ -825,10 +825,10 @@ def convert_excel_to_pdf(excel_path, pdf_path, sub_branch_cols_per_page=6, decla
     try:
         pdf.add_page()
         pdf.set_xy(10, pdf.h - 20)
-        pdf.set_font("Times", 'B', 12)
+        pdf.set_font("Times", 'B', 8)
         pdf.cell(0, 5, "CONTROLLER OF EXAMINATIONS", 0, 1, 'L')
         pdf.line(10, pdf.h - 15, 60, pdf.h - 15)
-        pdf.set_font("Times", size=12)
+        pdf.set_font("Times", size=9)
         pdf.set_xy(pdf.w - 30, pdf.h - 15)
         pdf.cell(20, 5, f"{pdf.page_no()} of {{nb}}", 0, 0, 'R')
 
